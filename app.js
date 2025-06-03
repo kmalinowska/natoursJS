5aef872df1522ce4d9fb3cf6c8418d4da8792236
Klaudia Malinowska <kmalinowska@protonmail.com>
2025-06-03 20:16:36 +0200

    feat(db): configure MongoDB connection with Mongoose

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`))
--- routes/server.js
+++ routes/server.js
@@ -1,5 +1,38 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('../app');
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+
+setupEnv();
+setupDBConnection(process.env.DB_USER, process.env.DB_PASS).catch(
+  console.error,
+);
+startServer(process.env.PORT || 3000);
+
+async function setupDBConnection(user, pass) {
+  const url = process.env.DB_URL.replace('<user>', user).replace(
+    '<password>',
+    pass,
+  );
+
+  const clientOptions = {
+    serverApi: { version: '1', strict: true, deprecationErrors: true },
+  };
+
+  try {
+    await mongoose.connect(url, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log('Connected to MongoDB');
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+function setupEnv() {
+  dotenv.config({ path: './.env' });
+}
+
+function startServer(port) {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
