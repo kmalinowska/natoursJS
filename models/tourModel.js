5228c1286b53b2e1dd86047f288bf98fd7745d87
Klaudia Malinowska <kmalinowska@protonmail.com>
2025-06-10 13:25:22 +0200

    feat: Add pre/post find middleware for secret tours & query timing

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -83,6 +87,18 @@ tourSchema.pre('save', function (next) {
 //   next();
 // });
 
+//QUERY MIDDLEWARE
+//tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
